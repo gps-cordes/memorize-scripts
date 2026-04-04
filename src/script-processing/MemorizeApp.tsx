ac6b76dd54d3c9ac1c4f0bdf9bccac6ef88a0dfc
Garrett Cordes <gps.cordes@gmail.com>
2026-04-04 15:48:08 -0500

    Change redaction level with buttons on main page

--- src/script-processing/MemorizeApp.tsx
+++ src/script-processing/MemorizeApp.tsx
@@ -8,11 +8,25 @@ export function MemorizeApp() {
   const [redacted, setRedacted] = useState(0)
 
   const displayScript = replaceWordsInText(patience_act_ii_gros_bunthorn, redacted)
+  // TODO: bold the first word of every new line before a colon to emphase character names
   // .replaceAll(/^(\w+):/, "<b>$1</b>")
 
+  function redactLess(){
+    if(redacted > 0){
+      setRedacted(redacted -1)
+    }
+  }
+  
+  function redactMore(){
+    setRedacted(redacted + 1)
+  }
 
   return (
-    <div style={{ whiteSpace: 'pre-line' }}>
+
+    <div style={{ whiteSpace: 'pre-line' }}>    
+    <button title="<" onClick={redactLess}>{'<'}</button>
+    <button title=">" onClick={redactMore}>{'>'}</button>  
+    <p>Redaction level: {redacted}</p>
       <p>
         {displayScript}
       </p>
