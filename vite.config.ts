0b637394fe23ba760ccb1744d28e4fc54ceffde6
Garrett Cordes <gps.cordes@gmail.com>
2026-04-06 20:11:02 -0500

    Show a QR code when app is started for mobile devices to visit easily

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,20 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import {qrcode} from 'vite-plugin-qrcode'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), qrcode()],
   test: {
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
     globals: true,
   },
+  server: {
+    host: true,
+    watch: {
+      usePolling: true, // Regularly checks files for changes
+    }
+  }
 })
